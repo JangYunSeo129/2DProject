e3ce9ada4d421a34c0961584ac6a5eee1314bfca
JangYunSeo129 <evilharu901@gmail.com>
2022-10-01 19:49:57 +0900

    changed asset and player move

--- project.py
+++ project.py
@@ -1,47 +1,83 @@
-from turtle import back, up
+from turtle import back, clear, up
 from pico2d import *
 
 os.getcwd()
 os.chdir('c:\\Users\\user\\Desktop\\tcu\\2-2\\2DGP\\2DProject')
-open_canvas(1536, 960)
-back_ground = load_image('Asset1\\artwork\\Environment\\back.png')
-tiles = load_image('Asset1\\artwork\\Environment\\dirt.png')
-player_run1 = load_image('Asset1\\artwork\\Sprites\\player\\run\\player-run-1.png')
-player_run2 = load_image('Asset1\\artwork\\Sprites\\player\\run\\player-run-2.png')
-player_run3 = load_image('Asset1\\artwork\\Sprites\\player\\run\\player-run-3.png')
-player_run4 = load_image('Asset1\\artwork\\Sprites\\player\\run\\player-run-4.png')
-player_run5 = load_image('Asset1\\artwork\\Sprites\\player\\run\\player-run-5.png')
-player_run6 = load_image('Asset1\\artwork\\Sprites\\player\\run\\player-run-6.png')
-player_jump1 = load_image('Asset1\\artwork\\Sprites\\player\\jump\\player-jump-1.png')
-player_jump2 = load_image('Asset1\\artwork\\Sprites\\player\\jump\\player-jump-2.png')
+open_canvas(1920, 1080)
+back_ground = load_image('background.png')
+player_sheet = load_image('player_sheet.png')
 
-def draw_back():
-    back_ground.clip_draw(0, 0, 384, 240, 768, 480, 1536, 960)
-    tiles.clip_draw(0, 140, 469, 313, 120, 53, 240, 106)
-    tiles.clip_draw(0, 140, 469, 313, 360, 53, 240, 106)
-    tiles.clip_draw(0, 140, 469, 313, 600, 53, 240, 106)
-    tiles.clip_draw(0, 140, 469, 313, 840, 53, 240, 106)
-    tiles.clip_draw(0, 140, 469, 313, 1080, 53, 240, 106)
-    tiles.clip_draw(0, 140, 469, 313, 1320, 53, 240, 106)
-    tiles.clip_draw(0, 140, 469, 313, 1560, 53, 240, 106)
-
+x, y = 400, 300
 def player_run(x, y):
-    player_run1.clip_draw(0, 0, 32, 32, x, y, 192, 192)
-    delay(0.06)
-    player_run2.clip_draw(0, 0, 32, 32, x, y, 192, 192)
-    delay(0.06)
-    player_run3.clip_draw(0, 0, 32, 32, x, y, 192, 192)
-    delay(0.06)
-    player_run4.clip_draw(0, 0, 32, 32, x, y, 192, 192)
-    delay(0.06)
-    player_run5.clip_draw(0, 0, 32, 32, x, y, 192, 192)
-    delay(0.06)
-    player_run6.clip_draw(0, 0, 32, 32, x, y, 192, 192)
-    delay(0.06)
+    for frame in range(6):
+        player_sheet.clip_draw(frame * 33, 32, 33, 32, x, y, 264, 256)
+        update_canvas()
+        delay(0.06)
+        clear_canvas()
 
-x, y = 400, 203
-draw_back()
-player_run(x, y)
-update_canvas()
-delay (2)
+def player_moveR(x, y):
+    for frame in range(6):
+        x = x + 35
+        player_sheet.clip_draw(frame * 33, 32, 33, 32, x, y, 264, 256)
+        update_canvas()
+        delay(0.06)
+        clear_canvas()
+    return x
+
+def player_moveL(x, y):
+    for frame in range(6):
+        x = x - 35
+        player_sheet.clip_draw(frame * 33, 32, 33, 32, x, y, 264, 256)
+        update_canvas()
+        delay(0.06)
+        clear_canvas()
+    return x
+
+def player_jump(x, y):
+    for i in range(5):
+        y = y + 71
+        player_sheet.clip_draw(0, 0, 33, 32, x, y, 264, 256)
+        update_canvas()
+        delay(0.06)
+        clear_canvas()
+    for i in range(3):
+        y = y - 33
+        player_sheet.clip_draw(33, 0, 32, 32, x, y, 264, 256)
+        update_canvas()
+        delay(0.06)
+        clear_canvas()
+    return y
+
+def player_down(x, y):
+    for i in range(8):
+        player_sheet.clip_draw(33, 0, 33, 32, x, y, 264, 256)
+        update_canvas()
+        delay(0.06)
+        y = y - 32
+        clear_canvas()
+    return y
+
+for i in range(5):
+    player_run(x, y)
+
+y = player_jump(x, y)
+
+for i in range(5):
+    player_run(x, y)
+
+y = player_down(x, y)
+
+for i in range(5):
+    player_run(x, y)
+
+x = player_moveR(x, y)
+
+for i in range(5):
+    player_run(x, y)
+
+x = player_moveL(x, y)
+
+for i in range(5):
+    player_run(x, y)
+
 close_canvas()
