3b42b83c1e8ff87e289d8f9b2565655f43b58bb8
JangYunSeo129 <evilharu901@gmail.com>
2022-11-16 23:51:00 +0900

    del
    쓸데없는거 삭제

--- project.py
+++
@@ -1,4 +0,0 @@
-inter = 5
-tax = 25
-money = int(input("저축금액을 입력하시오.: "))
-print("원금:    {0:d}\n이자:     {1:d}\n세금:      {2:d}\n최종:    {3:d}".format(money, money//20, (money//20)//4, money + money//20 -(money//20)//4))
